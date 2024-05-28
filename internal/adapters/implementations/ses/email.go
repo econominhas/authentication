aa467ddca2f15e4105871018dfebcfc71e6a7c6c
Henrique Leite <contact.henriqueleite42@gmail.com>
2024-05-28 10:44:57 -0300

    fix(adapters): fix aws context

--- internal/adapters/implementations/ses/email.go
+++ internal/adapters/implementations/ses/email.go
@@ -2,35 +2,52 @@ package ses
 
 import (
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/ses"
 	"github.com/aws/aws-sdk-go-v2/service/ses/types"
 	"github.com/econominhas/authentication/internal/adapters"
 )
 
-func (adp *SesAdapter) SendVerificationCodeEmail(i *adapters.SendVerificationCodeEmailInput) error {
-	senderEmail := "foo@bar.com"
-	charset := "utf-8"
-	subject := "Bem vindo ao Econominhas!"
-	body := "" + i.Code
+type sendEmailInput struct {
+	senderEmail   string
+	receiverEmail string
+	charset       string
+	subject       string
+	body          string
+}
 
-	_, err := adp.ses.SendEmail(context.TODO(), &ses.SendEmailInput{
+func (adp *SesAdapter) sendEmail(i *sendEmailInput) (*ses.SendEmailOutput, error) {
+	ctx, cf := context.WithTimeout(context.Background(), time.Second*5)
+	defer cf()
+
+	return adp.ses.SendEmail(ctx, &ses.SendEmailInput{
 		Destination: &types.Destination{
-			ToAddresses: []string{i.To},
+			ToAddresses: []string{i.receiverEmail},
 		},
-		Source: &senderEmail,
+		Source: &i.senderEmail,
 		Message: &types.Message{
 			Subject: &types.Content{
-				Charset: &charset,
-				Data:    &subject,
+				Charset: &i.charset,
+				Data:    &i.subject,
 			},
 			Body: &types.Body{
 				Html: &types.Content{
-					Charset: &charset,
-					Data:    &body,
+					Charset: &i.charset,
+					Data:    &i.body,
 				},
 			},
 		},
+	})
+}
+
+func (adp *SesAdapter) SendVerificationCodeEmail(i *adapters.SendVerificationCodeEmailInput) error {
+	_, err := adp.sendEmail(&sendEmailInput{
+		senderEmail:   "foo@bar.com",
+		receiverEmail: i.To,
+		charset:       "utf-8",
+		subject:       "Bem vindo ao Econominhas!",
+		body:          "" + i.Code,
 	})
 
 	return err
--- internal/adapters/implementations/sns/sms.go
+++ internal/adapters/implementations/sns/sms.go
@@ -2,17 +2,31 @@ package sns
 
 import (
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 	"github.com/econominhas/authentication/internal/adapters"
 )
 
-func (adp *SnsAdapter) SendVerificationCodeSms(i *adapters.SendVerificationCodeSmsInput) error {
-	body := "" + i.Code
+type sendSmsInput struct {
+	receiverPhoneNumber string
+	body                string
+}
 
-	_, err := adp.sns.Publish(context.TODO(), &sns.PublishInput{
-		PhoneNumber: &i.To,
-		Message:     &body,
+func (adp *SnsAdapter) sendSms(i *sendSmsInput) (*sns.PublishOutput, error) {
+	ctx, cf := context.WithTimeout(context.Background(), time.Second*5)
+	defer cf()
+
+	return adp.sns.Publish(ctx, &sns.PublishInput{
+		PhoneNumber: &i.receiverPhoneNumber,
+		Message:     &i.body,
+	})
+}
+
+func (adp *SnsAdapter) SendVerificationCodeSms(i *adapters.SendVerificationCodeSmsInput) error {
+	_, err := adp.sendSms(&sendSmsInput{
+		receiverPhoneNumber: i.To,
+		body:                "" + i.Code,
 	})
 
 	return err
